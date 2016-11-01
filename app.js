d2f0219304bd1a5886337cadeaf8b8c7fffb2859
Steve Larsen <stevelarsen01@gmail.com>
2016-11-01 17:26:57 -0500

    adding blocks to dom

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
-// javascript for base project
-// Created by: Steve Larsen
-// Created on: 2016-10-25
+//  jQuery Color Block Picker
+//  Developed by:  Steve Larsen
+//  Date: 11/01/2016
 
-console.log($("h1").val());
-console.log($(this));
+/*
+1. Create color array
+2. Create html including box canvas to hold blocks
+3. Create and append blocks from array
+4. Generate random number between 1 and total number of blocks
+5. Listen for block clicks
+6. Evaluate response
+7. Respond to guess
+
+*/
+
+var colorArray = ["AntiqueWhite", "Aqua", "Aquamarine", "Azure", "Beige", "Bisque", "Black", "BlanchedAlmond", "Blue", "BlueViolet", "Brown", "BurlyWood", "CadetBlue", "Chartreuse", "Chocolate", "Coral", "CornflowerBlue", "Cornsilk", "Crimson", "Cyan", "DarkBlue", "DarkCyan", "DarkGoldenRod", "DarkGray", "DarkGrey", "DarkGreen", "DarkKhaki", "DarkMagenta", "DarkOliveGreen", "DarkOrange", "DarkOrchid", "DarkRed", "DarkSalmon", "DarkSeaGreen", "DarkSlateBlue", "DarkSlateGray", "DarkSlateGrey", "DarkTurquoise", "DarkViolet", "DeepPink", "DeepSkyBlue", "DimGray", "DimGrey", "DodgerBlue", "FireBrick", "FloralWhite", "ForestGreen", "Fuchsia", "Gainsboro", "GhostWhite", "Gold", "GoldenRod", "Gray", "Grey", "Green", "GreenYellow", "HoneyDew", "HotPink", "IndianRed ", "Indigo ", "Ivory", "Khaki", "Lavender", "LavenderBlush", "LawnGreen", "LemonChiffon", "LightBlue", "LightCoral", "LightCyan", "LightGoldenRodYellow", "LightGray", "LightGrey", "LightGreen", "LightPink", "LightSalmon", "LightSeaGreen", "LightSkyBlue", "LightSlateGray", "LightSlateGrey", "LightSteelBlue", "LightYellow", "Lime", "LimeGreen", "Linen", "Magenta", "Maroon", "MediumAquaMarine", "MediumBlue", "MediumOrchid", "MediumPurple", "MediumSeaGreen", "MediumSlateBlue", "MediumSpringGreen", "MediumTurquoise", "MediumVioletRed", "MidnightBlue", "MintCream", "MistyRose", "Moccasin", "NavajoWhite", "Navy", "OldLace", "Olive", "OliveDrab", "Orange", "OrangeRed", "Orchid", "PaleGoldenRod", "PaleGreen", "PaleTurquoise", "PaleVioletRed", "PapayaWhip", "PeachPuff", "Peru", "Pink", "Plum", "PowderBlue", "Purple", "RebeccaPurple", "Red", "RosyBrown", "RoyalBlue", "SaddleBrown", "Salmon", "SandyBrown", "SeaGreen", "SeaShell", "Sienna", "Silver", "SkyBlue", "SlateBlue", "SlateGray", "SlateGrey", "Snow", "SpringGreen", "SteelBlue", "Tan", "Teal", "Thistle", "Tomato", "Turquoise", "Violet", "Wheat", "White", "WhiteSmoke", "Yellow",
+"YellowGreen"
+];
+
+$('document').ready(function () {
+
+  buildBlocks(colorArray);
+  function buildBlocks(colorArray) {
+
+    for (var i = 0; i < colorArray.length; i++) {
+      block = '<div class="block" name="' + colorArray[i] + '" style="background-color:' + colorArray[i] + '"></div>';
+      $("main").append(block);
+    }
+  }
+
+
+});
